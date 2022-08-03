a8cd427ab1fe069ac859df6a5ba604acb9a7f7db
rbrunney <rbrunney@student.neumont.edu>
2022-08-03 11:52:46 -0600

    Update ShopListUtil.java
    Made a method which will preform get request on on API's

--- WhatsForDinner/Microservices/Shopping List Service/ShoppingListService/src/main/java/com/whatsfordinner/shoppinglistservice/util/ShopListUtil.java
+++ WhatsForDinner/Microservices/Shopping List Service/ShoppingListService/src/main/java/com/whatsfordinner/shoppinglistservice/util/ShopListUtil.java
@@ -1,5 +1,10 @@
 package com.whatsfordinner.shoppinglistservice.util;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.time.LocalDateTime;
 import java.util.Base64;
 import java.util.HashMap;
@@ -31,5 +36,28 @@ public class ShopListUtil {
         //TODO Make a request to User service to check if they are a valid user
 
         return true;
+    }
+
+    // Making a Get Request
+    public static String getRequest(String urlLink) throws IOException {
+        //Starting Connection
+        URL url = new URL(urlLink);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+
+        //Setting up response formatting
+        connection.setRequestProperty("Accept", "application/json");
+
+        //Getting response data
+        BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String inputLine;
+        StringBuilder responseContent = new StringBuilder();
+        while ((inputLine = br.readLine()) != null) {
+            responseContent.append(inputLine);
+        }
+        br.close();
+        connection.disconnect();
+
+        return responseContent.toString();
     }
 }
