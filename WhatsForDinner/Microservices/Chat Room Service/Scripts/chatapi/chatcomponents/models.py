25318ba181e480af4160e3a3bc1994b6411302fa
DNgo-Neumont <78094165+DNgo-Neumont@users.noreply.github.com>
2022-08-07 13:44:33 -0600

    First bits of chatroom - still have to do the socketIO components and REDIS

---
+++ WhatsForDinner/Microservices/Chat Room Service/Scripts/chatapi/chatcomponents/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from rest_framework import serializers
+from djongo import models
+
+# Create your models here.
+
+class Person(models.Model):
+    userID = models.IntegerField(primary_key=True   )
+    name = models.CharField(max_length=2048)
+
+class Channels(models.Model):
+    _id = models.ObjectIdField()
+    groupName = models.CharField(max_length=2048)
+    invitedPeople = models.ArrayField(
+        model_container=Person
+    )
+
+class ChannelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Channels
+        fields = ["_id","invitedPeople"]
+
+class PersonSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Person
+        fields = ["userID","name"]
