14c2d6abf1fa1824014b0ed47f01e55abcd2c6b3
rbrunney <rbrunney@student.neumont.edu>
2022-08-11 08:19:50 -0600

    Meal Card Visual Done
    Meal Card Visual Done with everything, just have to add the back

--- WhatsForDinner/Front-End/WhatsForDinnerApp/whats_for_dinner/components/pages/Home/meal_card.dart
+++ WhatsForDinner/Front-End/WhatsForDinnerApp/whats_for_dinner/components/pages/Home/meal_card.dart
@@ -28,64 +28,107 @@ class _MealCard extends State<MealCard> {
   @override
   Widget build(BuildContext context) {
     return ConstrainedBox(
-      constraints:
-          const BoxConstraints(maxWidth: 350),
-      child: Card(
-        margin: const EdgeInsets.all(10),
-        elevation: 2,
-        child: Column(
-          children: [
-            const Text('Name of Meal Here'),
-            const Image(
-              image: NetworkImage(
-              'https://i.pinimg.com/736x/ba/92/7f/ba927ff34cd961ce2c184d47e8ead9f6.jpg')
-            ),
-            const Text(
-              'This is a test description...',
-            ),
-            Container(
-              alignment: Alignment.center,
-              child: Row(
-                children: [
-                  Expanded(
-                    flex: 1,
-                    child: IconButton(
-                      icon: Icon(_liked ? Icons.thumb_up : Icons.thumb_up_outlined),
-                      onPressed: () {
-                        setState(() {
-                          //need to make request dependent on state
-                          _liked = !_liked;
-                        });
-                      },
-                    ),
-                  ),
-                  const Expanded(
-                    flex: 1,
-                    child: Text(
-                      'Likes Go Here',
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
-                  Expanded(
-                    flex: 1,
-                    child: IconButton(
-                      icon: Icon(_bookMarked
-                          ? Icons.bookmark
-                          : Icons.bookmark_add_outlined),
-                      onPressed: () {
-                        setState(() {
-                          //need to make request dependent on state
-                          _bookMarked = !_bookMarked;
-                        });
-                      },
-                    ),
-                  ),
-                ]
-              ),
-            ),
-          ],
+        constraints: const BoxConstraints(
+          maxWidth: 350,
         ),
-      ),
-    );
+        child: GestureDetector(
+          onTap: () {
+            Navigator.push(context, MaterialPageRoute<void>(
+              builder: (BuildContext context) {
+                return Scaffold(
+                  body: Center(
+                    child: TextButton(
+                      child: Text('Icon Needs to Go Here'),
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                    ),
+                  ),
+                );
+              },
+            ));
+          },
+          child: Card(
+            margin: const EdgeInsets.all(10),
+            elevation: 2,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Column(
+              children: [
+                ClipRRect(
+                  borderRadius: const BorderRadius.only(
+                      topLeft: Radius.circular(10),
+                      topRight: Radius.circular(10)),
+                  child: Image.network(
+                    "https://i.pinimg.com/736x/ba/92/7f/ba927ff34cd961ce2c184d47e8ead9f6.jpg",
+                    width: 350,
+                    height: 150,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Container(
+                  alignment: Alignment.centerLeft,
+                  margin: const EdgeInsets.only(left: 15, top: 5, bottom: 5),
+                  child: const Text(
+                    'Name of Meal Here',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Container(
+                  alignment: Alignment.centerLeft,
+                  padding: const EdgeInsets.only(
+                    left: 15,
+                  ),
+                  child: const Text(
+                    'This is a test description...',
+                  ),
+                ),
+                Container(
+                  alignment: Alignment.center,
+                  child: Row(children: [
+                    Expanded(
+                      flex: 1,
+                      child: IconButton(
+                        icon: Icon(
+                            _liked ? Icons.thumb_up : Icons.thumb_up_outlined),
+                        onPressed: () {
+                          setState(() {
+                            //need to make request dependent on state
+                            _liked = !_liked;
+                          });
+                        },
+                      ),
+                    ),
+                    const Expanded(
+                      flex: 1,
+                      child: Text(
+                        '0',
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                    Expanded(
+                      flex: 1,
+                      child: IconButton(
+                        icon: Icon(_bookMarked
+                            ? Icons.bookmark
+                            : Icons.bookmark_add_outlined),
+                        onPressed: () {
+                          setState(() {
+                            //need to make request dependent on state
+                            _bookMarked = !_bookMarked;
+                          });
+                        },
+                      ),
+                    ),
+                  ]),
+                ),
+              ],
+            ),
+          ),
+        ));
   }
 }
