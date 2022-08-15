bc61bc4be57b0ab9a5e1938d1ae9093ddb527605
DNgo-Neumont <78094165+DNgo-Neumont@users.noreply.github.com>
2022-08-15 11:00:19 -0600

    Updated reqs, venv files, and added eureka to users

--- WhatsForDinner/Microservices/Chat Room Service/Scripts/activate.bat
+++ WhatsForDinner/Microservices/Chat Room Service/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\dngo\OneDrive - Neumont College of Computer Science\Neumont\Quarter 8\PRO290\WhatsForDinner\WhatsForDinner\WhatsForDinner\Microservices\Chat Room Service
+set VIRTUAL_ENV=C:\Users\dngo\Documents\PRO290\WhatsForDinner\WhatsForDinner\Microservices\Chat Room Service
 
 if not defined PROMPT set PROMPT=$P$G
 
