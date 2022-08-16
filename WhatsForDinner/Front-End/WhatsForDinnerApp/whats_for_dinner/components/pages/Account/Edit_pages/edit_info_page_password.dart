ce38599a7a2b1c1e86be2703b4f00bba5dce78da
Scipion2002 <79728249+Scipion2002@users.noreply.github.com>
2022-08-16 13:28:46 -0600

    Changed the settings layout
    Made it more dynamic and the password editing is more secure

---
+++ WhatsForDinner/Front-End/WhatsForDinnerApp/whats_for_dinner/components/pages/Account/Edit_pages/edit_info_page_password.dart
@@ -0,0 +1,138 @@
+import 'package:flutter/material.dart';
+
+class EditInfoPagePassword extends StatelessWidget {
+  const EditInfoPagePassword({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    TextEditingController oldPasswordController = TextEditingController();
+    TextEditingController newPasswordController = TextEditingController();
+    TextEditingController confirmNewPasswordController =
+        TextEditingController();
+    return SafeArea(
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                alignment: Alignment.centerLeft,
+                child: IconButton(
+                  icon: const Icon(Icons.arrow_back_outlined),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                ),
+              ),
+              Column(
+                children: [
+                  const Center(
+                    heightFactor: 5,
+                    child: Text(
+                      "Edit password",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Container(
+                    margin:
+                        const EdgeInsets.symmetric(horizontal: 15, vertical: 30),
+                    child: TextField(
+                        obscureText: true,
+                        controller: oldPasswordController,
+                        decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Old password')),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.symmetric(horizontal: 15),
+                    child: TextField(
+                      obscureText: true,
+                      controller: newPasswordController,
+                      decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'New password'),
+                    ),
+                  ),
+                  Container(
+                    margin:
+                        const EdgeInsets.symmetric(horizontal: 15, vertical: 30),
+                    child: TextField(
+                      obscureText: true,
+                      controller: confirmNewPasswordController,
+                      decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Confirm password'),
+                      onSubmitted: (String value) async {
+                        if (oldPasswordController.text != "poopyhead") {
+                          await showDialog<void>(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return AlertDialog(
+                                title: const Text('Old password was not correct'),
+                                content:
+                                    const Text('Your old password did not match'),
+                                actions: <Widget>[
+                                  TextButton(
+                                    onPressed: () {
+                                      Navigator.pop(context);
+                                    },
+                                    child: const Text('OK'),
+                                  ),
+                                ],
+                              );
+                            },
+                          );
+                        } else if (newPasswordController.text !=
+                            confirmNewPasswordController.text) {
+                          await showDialog<void>(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return AlertDialog(
+                                title: const Text('New password did not match'),
+                                content: const Text(
+                                    'Your new password did not matched with the confirm password'),
+                                actions: <Widget>[
+                                  TextButton(
+                                    onPressed: () {
+                                      Navigator.pop(context);
+                                    },
+                                    child: const Text('OK'),
+                                  ),
+                                ],
+                              );
+                            },
+                          );
+                        } else {
+                          await showDialog<void>(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return AlertDialog(
+                                title: const Text('Password has been Edited'),
+                                content: Text(
+                                    'Your password now has been changed to $value'),
+                                actions: <Widget>[
+                                  TextButton(
+                                    onPressed: () {
+                                      Navigator.pop(context);
+                                    },
+                                    child: const Text('OK'),
+                                  ),
+                                ],
+                              );
+                            },
+                          );
+                        }
+                        oldPasswordController.clear();
+                        newPasswordController.clear();
+                        confirmNewPasswordController.clear();
+                      },
+                    ),
+                  )
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
