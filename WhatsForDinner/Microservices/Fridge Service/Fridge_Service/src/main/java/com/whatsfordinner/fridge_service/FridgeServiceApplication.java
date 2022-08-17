de89948a9c02b393044843af55c443f928620de3
DNgo-Neumont <78094165+DNgo-Neumont@users.noreply.github.com>
2022-08-17 17:05:26 -0600

    Added dockerization components to Fridge API

--- WhatsForDinner/Microservices/Fridge Service/Fridge_Service/src/main/java/com/whatsfordinner/fridge_service/FridgeServiceApplication.java
+++ WhatsForDinner/Microservices/Fridge Service/Fridge_Service/src/main/java/com/whatsfordinner/fridge_service/FridgeServiceApplication.java
@@ -2,9 +2,16 @@ package com.whatsfordinner.fridge_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class FridgeServiceApplication {
+public class FridgeServiceApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+		return application.sources(FridgeServiceApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(FridgeServiceApplication.class, args);
