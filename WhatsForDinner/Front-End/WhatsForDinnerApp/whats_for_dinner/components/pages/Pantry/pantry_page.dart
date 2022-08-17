68889e96f78aade99c55d334fc1b8f8750eaa18e
Scipion2002 <79728249+Scipion2002@users.noreply.github.com>
2022-08-17 10:42:06 -0600

    Pantry icon and page added
    It took way too long to get one Icon but it worked!

---
+++ WhatsForDinner/Front-End/WhatsForDinnerApp/whats_for_dinner/components/pages/Pantry/pantry_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PantryPage extends StatelessWidget {
+  const PantryPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('Pantry Page');
+  }
+}
