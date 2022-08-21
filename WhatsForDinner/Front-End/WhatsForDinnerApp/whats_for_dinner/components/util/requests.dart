10115e16d46df43610613b398dbe87104f72f73f
Scipion2002 <79728249+Scipion2002@users.noreply.github.com>
2022-08-21 15:48:26 -0600

    Pantry page has ingredients
    All it needs is the add an ingredient page and be able to increase and decrease the qty of each ingredient by clicking the buttons

--- WhatsForDinner/Front-End/WhatsForDinnerApp/whats_for_dinner/components/util/requests.dart
+++ WhatsForDinner/Front-End/WhatsForDinnerApp/whats_for_dinner/components/util/requests.dart
@@ -2,7 +2,7 @@ import 'package:http/http.dart';
 
 class Requests {
   Future<void> makePostRequest(
-      String url, Map<String, dynamic> requestBody) async {
+    String url, Map<String, dynamic> requestBody) async {
     final requestLink = Uri.parse(url);
     final headers = {"Content-type": "application/json"};
     final json = requestBody.toString();
