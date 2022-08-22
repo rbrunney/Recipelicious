c0f7fb1905e262589a0e25c3ce1b5b5bb28b75c7
DNgo-Neumont <78094165+DNgo-Neumont@users.noreply.github.com>
2022-08-22 09:35:42 -0600

    Updated all the dockerfiles and docker compose to run smoothly

--- WhatsForDinner/Microservices/User Service/DjangoUsers/Scripts/DjangoUser/DjangoUser/__init__.py
+++ WhatsForDinner/Microservices/User Service/DjangoUsers/Scripts/DjangoUser/DjangoUser/__init__.py
@@ -1,24 +1,39 @@
+from time import sleep
+import traceback
 import mysql.connector
 import py_eureka_client.eureka_client as eureka_client
 import os
 
 #gotta change to the target mysqlcontainer
 
-connection = mysql.connector.connect(user=os.environ.get("MYSQL_USER"), password=os.environ.get("MYSQL_PASS"), host=os.environ.get("MYSQL_HOST"), port=int(os.environ.get("MYSQL_PORT")))
+print(os.environ.get("MYSQL_PASS"))
 
-cursor = connection.cursor()
+init_sql_ran = False
 
-cursor.execute(
-    "CREATE DATABASE IF NOT EXISTS djangousers"
-)
+while(not init_sql_ran):
 
-#Stopgap measure for create usercomponent's tables
-cursor.execute(
-    "USE djangousers; CREATE TABLE IF NOT EXISTS usercomponents_user ( id BIGINT NOT NULL AUTO_INCREMENT, name varchar(1024), username varchar(1024), password varchar(5000), email varchar(6000), birthday date, PRIMARY KEY(id))"
-)
+    try:
+        connection = mysql.connector.connect(user=os.environ.get("MYSQL_USER"), password=os.environ.get("MYSQL_PASS"), host=os.environ.get("MYSQL_HOST"), port=int(os.environ.get("MYSQL_PORT")))
 
-cursor.close()
-connection.close()
+        cursor = connection.cursor()
+        
+        cursor.execute(
+            "CREATE DATABASE IF NOT EXISTS djangousers"
+        )
+
+        #Stopgap measure for create usercomponent's tables
+        cursor.execute(
+            "USE djangousers; CREATE TABLE IF NOT EXISTS usercomponents_user ( id BIGINT NOT NULL AUTO_INCREMENT, name varchar(1024), username varchar(1024), password varchar(5000), email varchar(6000), birthday date, PRIMARY KEY(id))"
+        )
+        init_sql_ran = True
+        cursor.close()
+        connection.close()
+    except Exception as e:
+        print("Error with sql init: ")
+        traceback.print_exc(limit=20)
+        sleep(5)
+
+
 
 #port this server runs on
 serverPort = 8000
