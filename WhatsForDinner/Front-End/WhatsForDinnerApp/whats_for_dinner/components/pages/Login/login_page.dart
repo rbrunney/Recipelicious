9fd277d6beda74ff34aa3c198859c1436e4162ff
rbrunney <rbrunney@student.neumont.edu>
2022-08-22 23:13:35 -0600

    Requests For Login Page Done
    Just have to add for forgot password page to update the password and send email code to it

--- WhatsForDinner/Front-End/WhatsForDinnerApp/whats_for_dinner/components/pages/Login/login_page.dart
+++ WhatsForDinner/Front-End/WhatsForDinnerApp/whats_for_dinner/components/pages/Login/login_page.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:page_transition/page_transition.dart';
 import '../../util/page_navigation.dart';
+import '../../util/requests.dart';
+import 'alert_pop_up.dart';
 import 'create_account.dart';
 import 'forgot_password_page.dart';
+import 'dart:convert';
+import '../../util/globals.dart' as globals;
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -11,6 +15,7 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     TextEditingController _usernameController = TextEditingController();
     TextEditingController _passwordController = TextEditingController();
+    Requests requests = Requests();
 
     return SafeArea(
       child: Scaffold(
@@ -29,13 +34,13 @@ class LoginPage extends StatelessWidget {
                 child: TextField(
                   controller: _usernameController,
                   decoration: const InputDecoration(
-                      prefixIcon: Icon(Icons.account_circle_outlined, color: Colors.grey),
+                      prefixIcon: Icon(Icons.account_circle_outlined,
+                          color: Colors.grey),
                       focusedBorder: OutlineInputBorder(
                         borderSide: BorderSide(color: Colors.tealAccent),
                       ),
                       enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.grey)
-                      ),
+                          borderSide: BorderSide(color: Colors.grey)),
                       hintText: 'Enter Username...',
                       labelText: 'Enter Username',
                       labelStyle: TextStyle(color: Colors.grey)),
@@ -49,8 +54,7 @@ class LoginPage extends StatelessWidget {
                         borderSide: BorderSide(color: Colors.tealAccent),
                       ),
                       enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.grey)
-                      ),
+                          borderSide: BorderSide(color: Colors.grey)),
                       prefixIcon: Icon(Icons.lock_outline, color: Colors.grey),
                       hintText: 'Enter Password...',
                       labelText: 'Enter Password',
@@ -79,7 +83,7 @@ class LoginPage extends StatelessWidget {
                                 child: const Text(
                                   'Forgot Password?',
                                   style: TextStyle(
-                                    color: Colors.tealAccent,
+                                      color: Colors.tealAccent,
                                       decoration: TextDecoration.underline),
                                 ))))
                   ],
@@ -97,17 +101,39 @@ class LoginPage extends StatelessWidget {
                               primary: Colors.tealAccent, // background
                             ),
                             onPressed: () {
-                              Navigator.push(
-                                  context,
-                                  PageTransition(
-                                    type: PageTransitionType.fade,
-                                    child: const PageNavigation(),
-                                  ));
-                              FocusScopeNode currentFocus =
-                                  FocusScope.of(context);
-                              if (!currentFocus.hasPrimaryFocus) {
-                                currentFocus.unfocus();
-                              }
+                              requests
+                                  .makeGetRequest(
+                                      "http://10.0.2.2:8888/users/passwordCheck/${_usernameController.text}/${_passwordController.text}")
+                                  .then((value) {
+                                print(value);
+                                if (json.decode(value)["result"]) {
+                                  globals.username = _usernameController.text;
+                                  globals.password = _passwordController.text;
+                                  globals.isLoggedIn = true;
+
+                                  Navigator.push(
+                                      context,
+                                      PageTransition(
+                                        type: PageTransitionType.fade,
+                                        child: const PageNavigation(),
+                                      ));
+                                  FocusScopeNode currentFocus =
+                                      FocusScope.of(context);
+                                  if (!currentFocus.hasPrimaryFocus) {
+                                    currentFocus.unfocus();
+                                  }
+                                } else {
+                                  showDialog<void>(
+                                      context: context,
+                                      builder: (BuildContext context) {
+                                        return AlertPopUp(
+                                          title: 'Login Failed',
+                                          content:
+                                              'Login Information was incorrect try again',
+                                        );
+                                      });
+                                }
+                              });
                             },
                             child: const Text(
                               'Log In',
